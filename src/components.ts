5db8dc2131ca8c514009d78d97287900938bc88d
lidachao111222 <chao.li@usask.ca>
2021-10-14 01:26:17 -0600

    v0.2.0

--- src/components.ts
+++ src/components.ts
@@ -9,3 +9,6 @@ import { defineCustomElements } from 'awesome-layout/loader';
 
 defineCustomElements();
 export const AwesomeContainer = /*@__PURE__*/createReactComponent<JSX.AwesomeContainer, HTMLAwesomeContainerElement>('awesome-container');
+export const AwesomeFlexCol = /*@__PURE__*/createReactComponent<JSX.AwesomeFlexCol, HTMLAwesomeFlexColElement>('awesome-flex-col');
+export const AwesomeFlexGrid = /*@__PURE__*/createReactComponent<JSX.AwesomeFlexGrid, HTMLAwesomeFlexGridElement>('awesome-flex-grid');
+export const AwesomeFlexRow = /*@__PURE__*/createReactComponent<JSX.AwesomeFlexRow, HTMLAwesomeFlexRowElement>('awesome-flex-row');
