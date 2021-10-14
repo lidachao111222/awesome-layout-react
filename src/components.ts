532fe78d4ee65280305fbdc533a70f4cc5641a48
Sean Wong <seanwong24@live.com>
2021-10-14 00:42:51 -0600

    v0.1.1

---
+++ src/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'awesome-layout';
+
+import { defineCustomElements } from 'awesome-layout/loader';
+
+defineCustomElements();
+export const AwesomeContainer = /*@__PURE__*/createReactComponent<JSX.AwesomeContainer, HTMLAwesomeContainerElement>('awesome-container');
