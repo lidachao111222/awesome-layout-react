ea25c2eb792245ab71021f29d9da2e793b805f69
Sean Wong <seanwong24@live.com>
2021-10-30 02:56:04 -0600

    v0.7.0

--- src/components.ts
+++ src/components.ts
@@ -10,5 +10,7 @@ import { defineCustomElements } from 'awesome-layout/loader';
 defineCustomElements();
 export const AwesomeFlex = /*@__PURE__*/createReactComponent<JSX.AwesomeFlex, HTMLAwesomeFlexElement>('awesome-flex');
 export const AwesomeFlexItem = /*@__PURE__*/createReactComponent<JSX.AwesomeFlexItem, HTMLAwesomeFlexItemElement>('awesome-flex-item');
+export const AwesomeParallax = /*@__PURE__*/createReactComponent<JSX.AwesomeParallax, HTMLAwesomeParallaxElement>('awesome-parallax');
+export const AwesomeParallaxLayer = /*@__PURE__*/createReactComponent<JSX.AwesomeParallaxLayer, HTMLAwesomeParallaxLayerElement>('awesome-parallax-layer');
 export const AwesomeRatioBox = /*@__PURE__*/createReactComponent<JSX.AwesomeRatioBox, HTMLAwesomeRatioBoxElement>('awesome-ratio-box');
 export const AwesomeWrap = /*@__PURE__*/createReactComponent<JSX.AwesomeWrap, HTMLAwesomeWrapElement>('awesome-wrap');
